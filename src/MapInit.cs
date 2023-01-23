d827e5720623b4e5a638fe24714f54b734fe457f
ebertrand <ebertrand@gaming.tech>
2023-01-23 16:54:42 +0100

    map in main

---
+++ src/MapInit.cs
@@ -0,0 +1,46 @@
+﻿using System.Numerics;
+
+namespace Projet_7
+{
+    internal class MapInit
+    {
+        public char[,] tab = new char[9, 9];
+
+        public void Reset()
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+
+        public void InitTab()
+        {
+            for (var i = 0; i < tab.GetLength(0); i++)
+            {
+                for (var a = 0; a < tab.GetLength(1); a++)
+                {
+                    tab[i, a] = 'o';
+                }
+            }
+        }
+
+        public void PrintTab()
+        {         
+            tab[2, 0] = 'x';
+
+            for (var l = 0; l < tab.GetLength(0); l++)
+            {
+                for (var c = 0; c < tab.GetLength(1); c++)
+                {
+                    if (tab[l, c] == 'x')
+                    {
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    }
+                    Console.Write(tab[l, c]);
+                    Reset();
+                }
+                Console.WriteLine("t");
+            }
+        }
+    }
+}
---
+++ src/main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projet_7.src;
+
+namespace Projet_7.src
+{
+    public class main
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+            MapInit mi = new MapInit();
+            mi.InitTab();
+            mi.PrintTab();
+        }
+    }
+}
